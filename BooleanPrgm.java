2653744ed629783b87c5ed96d834e2b57542c50a
shwetaodel <79853762+shwetaodel@users.noreply.github.com>
2021-07-11 20:25:23 +0530

    Add files via upload

---
+++ BooleanPrgm.java
@@ -0,0 +1,26 @@
+package questions;
+
+public class BooleanPrgm {
+
+	public static void main(String[] args) {
+
+		boolean Do=true;
+		boolean Dont=false;
+		
+		System.out.println(Do);
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println(a>b);
+		
+		System.out.println(50>20);
+
+		System.out.println(10==10);
+	//	System.out.println((10).equalto);
+
+
+
+	}
+
+}
