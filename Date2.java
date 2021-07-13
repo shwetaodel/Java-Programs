4d09a097eb8a827ba85802a54bd97cb2c88a0888
shwetaodel <79853762+shwetaodel@users.noreply.github.com>
2021-07-13 21:13:35 +0530

    Add files via upload

---
+++ Date2.java
@@ -0,0 +1,13 @@
+package core;
+import java.time.LocalTime;
+
+public class Date2 {
+
+	public static void main(String[] args) {
+
+		LocalTime date = LocalTime.now();
+		System.out.println(date);
+		
+	}
+
+}
---
+++ Date3.java
@@ -0,0 +1,13 @@
+package core;
+import java.time.LocalDateTime;
+
+public class Date3 {
+
+	public static void main(String[] args) {
+
+		LocalDateTime date = LocalDateTime.now();
+		System.out.println(date);
+		
+	}
+
+}
